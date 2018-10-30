2d3fe9e1cd0fa6e4d0f477c125e480bef248e008
Rudakova Mariya <mariyars11@gmail.com>
2018-10-30 13:59:36 +0200

    first commit

---
+++ AndroidBasics_UserInterface/app/src/main/java/com/example/mariy/androidbasics_userinterface/HappyBirthday.java
@@ -0,0 +1,14 @@
+package com.example.mariy.androidbasics_userinterface;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class HappyBirthday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.happy_birthday);
+    }
+}
